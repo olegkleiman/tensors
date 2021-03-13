2eade395da6dd6d0ffd046f94076eae49a6fca1d
olegkleiman <oleg_kleyman@hotmail.com>
2021-03-13 15:00:51 +0200

    Additional axe for widgets

--- sigmoid.py
+++ sigmoid.py
@@ -9,27 +9,33 @@ current_weight = initial_weight
 current_bias = initial_bias
 
 sigmoid = lambda x, w, b: 1. / (1. + np.exp(-(x * w + b)))
-x_input = np.linspace(-2., 2, 100)
+line = lambda x, w, b: x * w + b
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.25)
-ax.margins(x=0)
+x_input = np.linspace(-2., 2, 100)
 
 axcolor = 'lightgoldenrodyellow'
 
-plt.axes(ax)  # select sin_ax
-plt.title(r'$y=\frac{1}{1 + e^{-(wx+b)}}$') # 'y = sigmoid(x, weight, bias)')
-sigmoid_plot, = plt.plot(x_input, sigmoid(x_input, initial_weight, initial_bias))
+fig = plt.figure(figsize=(5, 7))
+plt.subplots_adjust(left=0.25, bottom=-0.1)
+ax = fig.add_subplot(3, 1, 1)
+ax.set_title(r'$y=\frac{1}{1 + e^{-(wx+b)}}$') # 'y = sigmoid(x, weight, bias)')
+sigmoid_plot = ax.plot(x_input, sigmoid(x_input, initial_weight, initial_bias))
+ax = fig.add_subplot(3, 1, 2)
+ax.set_title(r'$y=wx+b$')
+line_plot = ax.plot(x_input, line(x_input, initial_weight, initial_bias), color='red')
 
-slider_bias_ax = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-slider_weight_ax = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+ax_bias = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+slider_bias_ax = plt.axes(ax_bias, facecolor=axcolor)
+ax_weight = fig.add_axes([0.25, 0.15, 0.65, 0.03])
+slider_weight_ax = plt.axes(ax_weight, facecolor=axcolor)
 
 bias_slider = Slider(slider_bias_ax, 'bias', -10., 10., valinit=initial_bias)
 weight_slider = Slider(slider_weight_ax, 'weight', -100., 100., valinit=initial_weight)
 
 
 def change_plot():
-    sigmoid_plot.set_ydata(sigmoid(x_input, current_weight, current_bias))
+    sigmoid_plot[0].set_ydata(sigmoid(x_input, current_weight, current_bias))
+    line_plot[0].set_ydata(line(x_input, current_weight, current_bias))
     fig.canvas.draw_idle()
 
 
@@ -43,7 +49,6 @@ def weight_update(new_weight):
     global current_weight
     current_weight = new_weight
     change_plot()
-
 
 bias_slider.on_changed(update)
 weight_slider.on_changed(weight_update)
